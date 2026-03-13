06ef6b910b5279de134ca86035962d8d934bcd71
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-03-13 12:50:45 +0800

    nigana ni the first try

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/png" href="{{ asset('A.png') }}">
-    <link rel="apple-touch-icon" href="{{ asset('A.png') }}">
+    <link rel="icon" type="image/png" href="{{ Vite::asset('resources/js/assets/company-logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ Vite::asset('resources/js/assets/company-logo.png') }}">
     <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
     @viteReactRefresh
     @vite('resources/js/app.jsx')
